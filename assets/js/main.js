848a4608944e781770c04df9521544265ddfaf63
inno404 <innocentmukabwa@gmail.com>
2023-02-07 16:00:36 +0300

    commited changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,67 @@
-// HAMBURGER
-var hamburger = document.querySelector(".hamburger");
-var menuitems = document.querySelector(".menuitems");
-hamburger.addEventListener("click",function(){
-    hamburger.classList.toggle("active")
-    menuitems.classList.toggle("active")
-})
+$(document).ready(()=>{
+    // hamburger click button
+    $('.hamburger').on("click", ()=>{
+        $(".hamburger").toggleClass("active")
+        $(".menuitems").toggleClass("active")
+
+    })
+
+    // missing code
 
 
-// hamburger disappears when tappped outside the box
-menuItems = document.querySelectorAll(".menuItems");
-menuItems.forEach(element => element.addEventListener('click',()=>{
-    hamburger.classList.remove("active")
-    menuitems.classList.remove("active") 
-}
-));
+    // marketplace
+    var products = [
+        {name:"Lorem ipsum",image:"../assets/img/5.jpg",price:10},
+        {name:"Lorem ipsum",image:"../assets/img/1.jpg",price:10},
+        {name:"Lorem ipsum",image:"../assets/img/2.jpg",price:10},
+        {name:"Lorem ipsum",image:"../assets/img/4.jpg",price:10},
+        {name:"Lorem ipsum",image:"../assets/img/7.jpg",price:10},
+        {name:"Lorem ipsum",image:"../assets/img/8.jpg",price:10},
+        {name:"Lorem ipsum",image:"../assets/img/1.jpg",price:10},
+        {name:"Lorem ipsum",image:"../assets/img/2.jpg",price:10},
+        {name:"Lorem ipsum",image:"../assets/img/4.jpg",price:10},
+        {name:"Lorem ipsum",image:"../assets/img/7.jpg",price:10},
+        {name:"Lorem ipsum",image:"../assets/img/8.jpg",price:10},
+        {name:"Lorem ipsum",image:"../assets/img/1.jpg",price:10},
+        {name:"Lorem ipsum",image:"../assets/img/2.jpg",price:10},
+        {name:"Lorem ipsum",image:"../assets/img/4.jpg",price:10},
+        {name:"Lorem ipsum",image:"../assets/img/7.jpg",price:10},
+        {name:"Lorem ipsum",image:"../assets/img/8.jpg",price:10},
+        {name:"Lorem ipsum",image:"../assets/img/1.jpg",price:10},
+        {name:"Lorem ipsum",image:"../assets/img/2.jpg",price:10},
+        {name:"Lorem ipsum",image:"../assets/img/4.jpg",price:10},
+        {name:"Lorem ipsum",image:"../assets/img/7.jpg",price:10},
+        {name:"Lorem ipsum",image:"../assets/img/8.jpg",price:10}
+    ]
+    // LOOP THROUGH THE PRODUCTS AND RETURN EACH PRODUCT
+    let newProducts = products.map((item)=>{
+        return item
+    })
+    // RETURN HTML ELEMENTS IN THE DOM
+    $(".grid-container").html(newProducts.map(function(item){
+        var {image,name,price}=item
+        return(
+            `<div class="grid-content">
+            <img src=${image}>
+            <p style="color:white">${name}</p>
+            <div class="extras">
+                <span class="stars">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+
+                </span>
+                
+                <h4>${price}</h4>
+
+                <span class="cart">
+                    <i class="fa-solid fa-cart-plus"></i>
+                </span>
+            </div>
+            </div>
+            `
+        )
+    }))
+    console.log(newProducts)
+});
